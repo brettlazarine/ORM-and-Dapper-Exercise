1010adf57bf198909324354581117d48a8607285
Brett Lazarine <jblazarine@gmail.com>
2023-03-14 02:48:50 -0500

    complete exercise two, bonus tasks

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    internal class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");   
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _connection.QuerySingle<Product>("Select * FROM products WHERE ProductID = @id", new { id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _connection.Execute("UPDATE products SET Name = @name," +
+                " Price = @price," +
+                " CategoryID = @categoryID," +
+                " OnSale = @onsale," +
+                " StockLevel = @stock " +
+                "WHERE ProductID = @id;",
+                new { id = product.ProductID,
+                    name = product.Name,
+                    price = product.Price,
+                    categoryID = product.CategoryID,
+                    onsale = product.OnSale,
+                    stock = product.StockLevel });
+        }
+
+        public void DeleteProduct(int id)
+        {
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @id;", new { id });
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @id;", new { id });
+            _connection.Execute("DELETE FROM products WHERE ProductID = @id;", new { id });
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -17,13 +17,28 @@ namespace ORM_Dapper
 
             IDbConnection conn = new MySqlConnection(connString);
 
-            var departmentRepo = new DapperDepartmentRepository(conn);
-            departmentRepo.InsertDepartment("NewDept");
-            var departments = departmentRepo.GetAllDepartments();
-            foreach (var department in departments)
-            {
-                Console.WriteLine($"{department.DepartmentID} {department.Name}");  
-            }
+            //var departmentRepo = new DapperDepartmentRepository(conn);
+            //departmentRepo.InsertDepartment("NewDept");
+            //var departments = departmentRepo.GetAllDepartments();
+            //foreach (var department in departments)
+            //{
+            //    Console.WriteLine($"{department.DepartmentID} {department.Name}");  
+            //}
+
+            var productRepo = new DapperProductRepository(conn);
+            //var productToUpdate = productRepo.GetProduct(940);
+            //productToUpdate.Name = "New Hotness";
+            //productToUpdate.StockLevel = 5;
+            //productToUpdate.Price = 999.99;
+            //productToUpdate.OnSale = false;
+            //productToUpdate.CategoryID = 2;
+            //productRepo.UpdateProduct(productToUpdate); 
+            //var products = productRepo.GetAllProducts();
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine($"{product.ProductID} {product.Name}: {product.Price}, {product.StockLevel}, {product.OnSale}");
+            //}
+            productRepo.DeleteProduct(940);
         }
     }
 }
